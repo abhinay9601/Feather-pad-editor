824074a695cb0dd17695614126765fc91166b200
abhinay9601 <abhinaysingh372@gmail.com>
2023-04-04 10:36:55 +0530

    added save functionalaty in web editor

--- src/Components/WebLang/Main.js
+++ src/Components/WebLang/Main.js
@@ -2,6 +2,7 @@ import "./Main.css";
 import { Link } from "react-router-dom";
 import { GiSpiderWeb } from "react-icons/gi";
 import { BiArrowBack } from "react-icons/bi";
+import { saveAs } from "file-saver";
 
 const MainWeb = () => {
   const compile = () => {
@@ -9,6 +10,8 @@ const MainWeb = () => {
     var css = document.getElementById("css");
     var js = document.getElementById("js");
     var code = document.getElementById("code").contentWindow.document;
+
+    console.log(html.value)
 
     document.body.onkeyup = function () {
       code.open();
@@ -24,6 +27,27 @@ const MainWeb = () => {
       code.close();
     };
   };
+  const saveHtml = () => {
+    var html = document.getElementById("html");
+    const fileToSave = new Blob([html.value], {
+      type: "text/plain;charset=utf-8",
+    });
+    saveAs(fileToSave, "example.html");
+  };
+  const saveCss = () => {
+    var css = document.getElementById("css");
+    const fileToSave = new Blob([css.value], {
+      type: "text/plain;charset=utf-8",
+    });
+    saveAs(fileToSave, "example.css");
+  };
+  const saveJs = () => {
+    var js = document.getElementById("js");
+    const fileToSave = new Blob([js.value], {
+      type: "text/plain;charset=utf-8",
+    });
+    saveAs(fileToSave, "example.js");
+  };
 
   // compile();
   return (
@@ -38,8 +62,7 @@ const MainWeb = () => {
             </span>{" "}
           </div>
           <div className="webname flex">
-            <GiSpiderWeb></GiSpiderWeb> {" "}
-             WEB EDITOR
+            <GiSpiderWeb></GiSpiderWeb> WEB EDITOR
           </div>
           <div className="innertopweb">
             <div>
@@ -61,16 +84,39 @@ const MainWeb = () => {
             <p>
               <span>{"/"}</span>HTML
             </p>
+            <div
+            onClick={saveHtml}
+              style={{width:"min-content"}}
+              className="font-bold border-2 py-1 px-3 mx-2 rounded border-black bg-green-600"
+            >
+              save
+            </div>
           </div>
           <div>
             <p>
               <span className="span2">{" * "}</span>CSS
             </p>
+            <div
+            onClick={saveCss}
+
+              style={{width:"min-content"}}
+              className="font-bold border-2 py-1 px-3 mx-2 rounded border-black bg-green-600"
+            >
+              save
+            </div>
           </div>
           <div>
             <p>
               <span className="span3">{"()"}</span>JavaScript
             </p>
+            <div
+            onClick={saveJs}
+
+              style={{width:"min-content"}}
+              className="font-bold border-2 py-1 px-3 mx-2 rounded border-black bg-green-600"
+            >
+              save
+            </div>
           </div>
         </div>
         <textarea onKeyUp={compile} id="html" placeholder="HTML"></textarea>

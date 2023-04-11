4ca7c67194e5e8e4c402905ebcc48130e76f2c21
abhinay9601 <abhinaysingh372@gmail.com>
2023-04-11 11:38:23 +0530

    added save function in register

--- src/Components/Signup/Main.js
+++ src/Components/Signup/Main.js
@@ -4,69 +4,90 @@ import { Link } from "react-router-dom";
 import { useState } from "react";
 const MainSignin = () => {
   const [phone, setphone] = useState("");
+  const [email, setEmail] = useState("");
+  // const [contactNo, setContactNo] = useState("");
+  const [password, setPassword] = useState("");
+
+  const emailHandler = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const passwordHandler = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const submitHandler = () => {
+    localStorage.setItem("Email",email);
+    localStorage.setItem("Passowrd",password)
+    localStorage.setItem("Phone",phone)
+
+  };
+
   return (
     <div className="outsideContainerlogin">
       <div className="maincontainer">
         <div className="monkeylogin">
           <div className="registertext">Register</div>
           <div className="formcon">
-            <form >
+            <form>
               <div className="form">
-              <div>
-                <input
-                  type="tel"
-                  id="mail"
-                  name="username"
-                  // onChange={userNameHandler}
-                  className="tb input"
-                  placeholder="Email"
-                  autoComplete="off"
-                />
-              </div>
-              
-              <br />
-              <div>
-                <PhoneInput
-                  className=""
-                  specialLabel={""}
-                  country={"in"}
-                  value={phone}
-                  onChange={(phone) => setphone(phone)}
-                />
-              </div>
-              <br />
-              <div>
-                <input
-                  type="password"
-                  id="pwdbar"
-                  // onClick={passclick}
-                  // onChange={passwordHandler}
-                  name="pwd"
-                  className="tb input"
-                  placeholder="Password"
-                />
-              </div>
-              <br />
-              <br />
-              <div>
-                <Link to="/">
-                  {" "}
+                <div>
                   <input
-                    type="submit"
-                    name=""
-                    // onClick={submitHandler}
-                    className="sbutton"
-                    value="R E G I S T E R"
+                    type="text"
+                    id="mail"
+                    name="username"
+                    onChange={emailHandler}
+                    className="tb input"
+                    placeholder="Email"
+                    autoComplete="off"
                   />
-                </Link>
-              </div>
-              <br />
-              <div className="regi">
-                <Link to="/login">
-                  {" "}
-                  <span className="registerdown">Already account? Login </span>
-                </Link>
-              </div>
+                </div>
+
+                <br />
+                <div>
+                  <PhoneInput
+                    className=""
+                    specialLabel={""}
+                    country={"in"}
+                    value={phone}
+                    onChange={(phone) => setphone(phone)}
+                  />
+                </div>
+                <br />
+                <div>
+                  <input
+                    type="password"
+                    id="pwdbar"
+                    // onClick={passclick}
+                    onChange={passwordHandler}
+                    name="pwd"
+                    className="tb input"
+                    placeholder="Password"
+                  />
+                </div>
+                <br />
+                <br />
+                <div>
+                  <Link to="/">
+                    {" "}
+                    <input
+                      type="submit"
+                      name=""
+                      onClick={submitHandler}
+                      className="sbutton"
+                      value="R E G I S T E R"
+                    />
+                  </Link>
+                </div>
+                <br />
+                <div className="regi">
+                  <Link to="/login">
+                    {" "}
+                    <span className="registerdown">
+                      Already account? Login{" "}
+                    </span>
+                  </Link>
+                </div>
               </div>
             </form>
           </div>
